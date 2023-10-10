4169cf674270b3f09239599ff9d95fc6bc4eb61c
root <root@DESKTOP-JGMP2PO>
2023-10-10 18:40:50 -0500

    [ADD]Custom test and eslint config

--- demos/src/index.js
+++ demos/src/index.js
@@ -12,4 +12,11 @@ function divide(a, b) {
   }
   return a / b;
 }
-module.exports = {sum,multiply,divide};
+
+function calcularPesoEnOtroPlaneta(peso, gravedadPlaneta) {
+  // La fórmula para calcular el peso en otro planeta es:
+  // peso_en_otro_planeta = peso_en_la_tierra * gravedad_del_otro_planeta
+  const pesoEnOtroPlaneta = peso * gravedadPlaneta;
+  return pesoEnOtroPlaneta;
+}
+module.exports = {sum,multiply,divide, calcularPesoEnOtroPlaneta};
--- demos/src/index.test.js
+++ demos/src/index.test.js
@@ -1,4 +1,4 @@
-const { sum, multiply, divide } = require("./index.js");
+const { sum, multiply, divide,calcularPesoEnOtroPlaneta } = require("./index.js");
 
 //test: el nombre del test que estamos corriendo
 
@@ -30,4 +30,12 @@ test("any  to be null", () => {
   const response3 = divide(20, 0);
   //expect: la hipotesis de lo que quiero
   expect(response3).toBeNull();
+});
+
+
+test("Tu peso en otro planeta (luna y marte)",()=>{
+    const gravedadMarte = 3.72076; // Gravedad en Marte
+    const gravedadLuna = 1.625; // Gravedad en la Luna
+    expect(calcularPesoEnOtroPlaneta(50,gravedadLuna)).toBe(81.25);
+    expect(calcularPesoEnOtroPlaneta(50,gravedadMarte)).toBe(186.03799999999998);
 });
