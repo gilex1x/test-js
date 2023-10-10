d450b9545ea5fbb1bc879f6f6ba98136b74b13db
root <root@DESKTOP-JGMP2PO>
2023-10-10 18:26:36 -0500

    [ADD]My first test

--- demos/src/index.js
+++ demos/src/index.js
@@ -1,4 +1,15 @@
 function sum(a, b) {
-    return a + b;
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if(b==0){
+    return null
   }
-  module.exports = sum;
+  return a / b;
+}
+module.exports = {sum,multiply,divide};
--- demos/src/index.test.js
+++ demos/src/index.test.js
@@ -1,5 +1,33 @@
-const sum = require('./index.js');
+const { sum, multiply, divide } = require("./index.js");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+//test: el nombre del test que estamos corriendo
+
+test("adds 3 + 2 to equal 5", () => {
+  const response = sum(3, 2);
+  //expect: la hipotesis de lo que quiero
+  expect(response).toBe(5);
+});
+
+test("adds 3 * 2 to equal 6", () => {
+  const response = multiply(3, 2);
+  //expect: la hipotesis de lo que quiero
+  expect(response).toBe(6);
+});
+
+test("20 / 4 to equal 5", () => {
+  const response = divide(20, 4);
+  //expect: la hipotesis de lo que quiero
+  expect(response).toBe(5);
+});
+
+test("any  to be null", () => {
+  const response = divide(20, 0);
+  //expect: la hipotesis de lo que quiero
+  expect(response).toBeNull();
+  const response2 = divide(20, 0);
+  //expect: la hipotesis de lo que quiero
+  expect(response2).toBeNull();
+  const response3 = divide(20, 0);
+  //expect: la hipotesis de lo que quiero
+  expect(response3).toBeNull();
 });
