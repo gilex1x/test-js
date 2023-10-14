4ff42f958621b0d1cc4b59b181658b29558b557b
root <root@DESKTOP-JGMP2PO>
2023-10-14 18:59:12 -0500

    [ADD]Api testing initial config

--- demos/src/matchers.test.js
+++ demos/src/matchers.test.js
@@ -1,11 +1,21 @@
-test('Test de un objeto', () => {
-  const data = { name: 'Gilbert' };
-  expect(data).toEqual({ name: 'Gilbert' });
+describe('Matchers Test', () => {
+  test('Test de un objeto', () => {
+    const data = { name: 'Gilbert' };
+    expect(data).toEqual({ name: 'Gilbert' });
+  });
+  test('Test de null', () => {
+    const data = null;
+    expect(data).toBeNull();
+    expect(data).toBeDefined();
+    expect(data).not.toBeUndefined();
+  });
 });
 
-test('Test de null', () => {
-  const data = null;
-  expect(data).toBeNull();
-  expect(data).toBeDefined();
-  expect(data).not.toBeUndefined();
+describe('Matchers Test: Falsy', () => {
+  test('Test de false', () => {
+    const data = false;
+    expect(data).toBeFalsy();
+    expect(0).toBeFalsy();
+    expect('').toBeFalsy();
+  });
 });
