7db82beccf46dcd06c11bf8bb9a4d7cc8cffbc08
root <root@DESKTOP-JGMP2PO>
2023-10-16 19:38:05 -0500

    [ADD]Initial moking and dummies config to test api

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,10 +1,32 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: {
+      $oid: '652b2da6811167ed66e5e48a',
+    },
+    name: 'Harry Potter',
+  },
+  {
+    _id: {
+      $oid: '652b2e0c811167ed66e5e48b',
+    },
+    name: 'Lord of the rings',
+  },
+];
+
+const mongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => mongoLibStub));
 describe('Pruebas para book service', () => {
   let services;
   beforeEach(() => {
     // Antes de cada prueba creamos una instancia del servicio
     services = new BooksService();
+    jest.clearAllMocks();
   });
 
   describe('Mthod getBooks', () => {
